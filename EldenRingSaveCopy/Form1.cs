69b6d17d1a9e5594536f87bdff5a4af382af28a8
Ben Green <ben.green@live.com.au>
2022-03-05 11:00:41 +1100

    Complete refactor. Added header copy for accurate load game information within the game on first load. Added header parsing to provide character names instead of slot names.

--- EldenRingSaveCopy/Form1.cs
+++ EldenRingSaveCopy/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.IO;
 using System.Security.Cryptography;
+using EldenRingSaveCopy.Saves.Model;
 
 namespace EldenRingSaveCopy
 {
@@ -19,7 +20,12 @@ namespace EldenRingSaveCopy
 
         public const int WM_NCLBUTTONDOWN = 0xA1;
         public const int HT_CAPTION = 0x2;
-        public const int SLOT_LENGTH = 0x280000;
+
+        private BindingList<ISaveGame> sourceSaveGames = new BindingList<ISaveGame>();
+        private BindingList<ISaveGame> targetSaveGames = new BindingList<ISaveGame>();
+
+        private ISaveGame selectedSourceSave = new NullSaveGame();
+        private ISaveGame selectedTargetSave = new NullSaveGame();
 
         [DllImportAttribute("user32.dll")]
         public static extern int SendMessage(IntPtr hWnd,
@@ -44,6 +50,7 @@ namespace EldenRingSaveCopy
 
         private void sourceFileBrowse(object sender, EventArgs e)
         {
+            sourceSaveGames.Clear();
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Elden Ring Save File |ER0000.sl2";
             DialogResult result = openFileDialog.ShowDialog(); // Show the dialog.
@@ -54,6 +61,16 @@ namespace EldenRingSaveCopy
                 {
                     _fileManager.SourceFile = File.ReadAllBytes(_fileManager.SourcePath);
                     sourceFilePath.Text = _fileManager.SourcePath;
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        var newSave = new SaveGame();
+                        newSave.LoadData(_fileManager.SourceFile, i);
+                        if(newSave.Active)
+                        {
+                            sourceSaveGames.Add(newSave);
+                        }
+                    }
                 }
                 catch (IOException)
                 {
@@ -63,9 +80,37 @@ namespace EldenRingSaveCopy
             CheckButtonState();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void targetButtonBrowse(object sender, EventArgs e)
         {
+            targetSaveGames.Clear();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Elden Ring Save File |ER0000.sl2";
+            DialogResult result = openFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                _fileManager.TargetPath = openFileDialog.FileName;
+                try
+                {
+                    _fileManager.TargetFile = File.ReadAllBytes(_fileManager.TargetPath);
+                    targetFilePath.Text = _fileManager.TargetPath;
 
+                    for (int i = 0; i < 10; i++)
+                    {
+                        var newSave = new SaveGame();
+                        newSave.LoadData(_fileManager.TargetFile, i);
+                        if (!newSave.Active)
+                        {
+                            newSave.CharacterName = $"Slot {i + 1}";
+                        }
+                        targetSaveGames.Add(newSave);
+                    }
+                }
+                catch (IOException)
+                {
+                    sourceFilePath.Text = "Failed to load";
+                }
+            }
+            CheckButtonState();
         }
 
         private void CheckButtonState()
@@ -73,7 +118,7 @@ namespace EldenRingSaveCopy
             copyButton.Text = "Copy";
             if (_fileManager.SourceFile.Length > 0 && _fileManager.TargetFile.Length > 0 
                 && _fileManager.SourcePath != _fileManager.TargetPath &&
-                _fileManager.SourceSlot != Saves.Slot.NoSelection && _fileManager.TargetSlot != Saves.Slot.NoSelection)
+                this.selectedSourceSave.Id != Guid.Empty && this.selectedTargetSave.Id != Guid.Empty)
             {
                 copyButton.Enabled = true;
                 copyButton.BackColor = Color.Lime;
@@ -85,125 +130,129 @@ namespace EldenRingSaveCopy
             }
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void targetButtonBrowse(object sender, EventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Elden Ring Save File |ER0000.sl2";
-            DialogResult result = openFileDialog.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                _fileManager.TargetPath = openFileDialog.FileName;
-                try
-                {
-                    _fileManager.TargetFile = File.ReadAllBytes(_fileManager.TargetPath);
-                    targetFilePath.Text = _fileManager.TargetPath;
-                }
-                catch (IOException)
-                {
-                    sourceFilePath.Text = "Failed to load";
-                }
-            }
-            CheckButtonState();
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             titleBar.MouseDown += Form1_MouseDown;
             _fileManager = new FileManager();
+
+            fromSaveSlot.DisplayMember = "CharacterName";
+            //fromSaveSlot.DataSource = this.sourceSaveGames;
+            fromSaveSlot.DataSource = new BindingSource() { DataSource = this.sourceSaveGames }.DataSource;
+            toSaveSlot.DisplayMember = "CharacterName";
+            //toSaveSlot.DataSource = this.targetSaveGames;
+            toSaveSlot.DataSource = new BindingSource() { DataSource = this.targetSaveGames }.DataSource;
         }
 
         private void fromSaveSlot_SelectedIndexChanged(object sender, EventArgs e)
         {
             var ComboBox = (ComboBox)sender;
-            switch (ComboBox.SelectedIndex)
-            {
-                case (0):
-                    _fileManager.SourceSlot = Saves.Slot.Slot1;
-                    break;
-                case (1):
-                    _fileManager.SourceSlot = Saves.Slot.Slot2;
-                    break;
-                case (2):
-                    _fileManager.SourceSlot = Saves.Slot.Slot3;
-                    break;
-                case (3):
-                    _fileManager.SourceSlot = Saves.Slot.Slot4;
-                    break;
-                case (4):
-                    _fileManager.SourceSlot = Saves.Slot.Slot5;
-                    break;
-                case (5):
-                    _fileManager.SourceSlot = Saves.Slot.Slot6;
-                    break;
-                case (6):
-                    _fileManager.SourceSlot = Saves.Slot.Slot7;
-                    break;
-                case (7):
-                    _fileManager.SourceSlot = Saves.Slot.Slot8;
-                    break;
-                case (8):
-                    _fileManager.SourceSlot = Saves.Slot.Slot9;
-                    break;
-                case (9):
-                    _fileManager.SourceSlot = Saves.Slot.Slot10;
-                    break;
-                default:
-                    _fileManager.SourceSlot = Saves.Slot.NoSelection;
-                    break;
-            }
+            this.selectedSourceSave = (ISaveGame)ComboBox.SelectedItem;
             CheckButtonState();
         }
 
         private void toSaveSlot_SelectedIndexChanged(object sender, EventArgs e)
         {
             var ComboBox = (ComboBox)sender;
-            switch (ComboBox.SelectedIndex)
-            {
-                case (0):
-                    _fileManager.TargetSlot = Saves.Slot.Slot1;
-                    break;
-                case (1):
-                    _fileManager.TargetSlot = Saves.Slot.Slot2;
-                    break;
-                case (2):
-                    _fileManager.TargetSlot = Saves.Slot.Slot3;
-                    break;
-                case (3):
-                    _fileManager.TargetSlot = Saves.Slot.Slot4;
-                    break;
-                case (4):
-                    _fileManager.TargetSlot = Saves.Slot.Slot5;
-                    break;
-                case (5):
-                    _fileManager.TargetSlot = Saves.Slot.Slot6;
-                    break;
-                case (6):
-                    _fileManager.TargetSlot = Saves.Slot.Slot7;
-                    break;
-                case (7):
-                    _fileManager.TargetSlot = Saves.Slot.Slot8;
-                    break;
-                case (8):
-                    _fileManager.TargetSlot = Saves.Slot.Slot9;
-                    break;
-                case (9):
-                    _fileManager.TargetSlot = Saves.Slot.Slot10;
-                    break;
-                default:
-                    _fileManager.TargetSlot = Saves.Slot.NoSelection;
-                    break;
-            }
+            this.selectedTargetSave = (ISaveGame)ComboBox.SelectedItem;
             CheckButtonState();
+        }
+
+        private void CreateFileBackup(string path, byte[] file)
+        {
+            int backupCount = 2;
+            string backupPath = path.Replace("ER0000.sl2", "ER0000.backup1");
+            while (File.Exists(backupPath))
+            {
+                backupPath = backupPath.Remove(backupPath.Length - 1, 1) + $"{backupCount}";
+                backupCount++;
+            }
+            File.WriteAllBytes(backupPath, file);
+        }
+
+        private int SlotStartIndex(SaveGame save)
+        {
+            return (SaveGame.SLOT_START_INDEX + (save.Index * 0x10) + (save.Index * SaveGame.SLOT_LENGTH));
+        }
+
+        private int HeaderStartIndex(SaveGame save)
+        {
+            return (SaveGame.SAVE_HEADER_START_INDEX + (save.Index * SaveGame.SAVE_HEADER_LENGTH));
+        }
+
+        private void copyButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CreateFileBackup(_fileManager.TargetPath, _fileManager.TargetFile);
+
+                var sourceSave = (SaveGame)this.selectedSourceSave;
+                var targetSave = (SaveGame)this.selectedTargetSave;
+                byte[] newSave = new byte[_fileManager.TargetFile.Length];
+
+                //Create temp working file
+                Array.Copy(_fileManager.TargetFile, newSave, _fileManager.TargetFile.Length);       
+
+                //Replace steam id in the source save with the steam id of the target save
+                foreach (int idLocation in sourceSave.SaveData.StartingIndex(_fileManager.SourceID))
+                {
+                    Array.Copy(_fileManager.TargetID, 0, sourceSave.SaveData, idLocation, _fileManager.TargetID.Length);
+                }
+
+                //Copy source save slot to target save slot in temp file
+                Array.Copy(sourceSave.SaveData, 0, newSave, SlotStartIndex(targetSave), SaveGame.SLOT_LENGTH);
+
+                //Copy save header to temp file
+                Array.Copy(sourceSave.HeaderData, 0, newSave, HeaderStartIndex(targetSave), SaveGame.SAVE_HEADER_LENGTH);
+
+                //Mark target slot as active
+                newSave[SaveGame.CHAR_ACTIVE_STATUS_START_INDEX + targetSave.Index] = 0x01;
+
+                //Calculate checksums
+                using (var md5 = MD5.Create())
+                {
+                    //Get slot checksum
+                    md5.ComputeHash(sourceSave.SaveData);
+                    //Write checksum to temp target file
+                    Array.Copy(md5.Hash, 0, newSave, SlotStartIndex(targetSave) - 0x10, 0x10);
+                    //get header checksum
+                    md5.ComputeHash(newSave.Skip(SaveGame.SAVE_HEADERS_SECTION_START_INDEX).Take(SaveGame.SAVE_HEADERS_SECTION_LENGTH).ToArray());
+                    //Write headers checksum
+                    Array.Copy(md5.Hash, 0, newSave, SaveGame.SAVE_HEADERS_SECTION_START_INDEX - 0x10, 0x10);
+                }
+
+                
+
+                //Write temp file to target file
+                File.WriteAllBytes(_fileManager.TargetPath, newSave);
+
+                //Delete old backup file to avoid corrupt save error
+                File.Delete(_fileManager.TargetPath + ".bak");
+
+
+                //Indicate successful copy
+                copyButton.Enabled = false;
+                copyButton.Text = "Complete";
+                copyButton.BackColor = Color.SeaGreen;
+            }
+            catch (Exception _e)
+            {
+                copyButton.Enabled = false;
+                copyButton.Text = "Failed";
+                copyButton.BackColor = Color.DarkRed;
+                byte[] err = Encoding.Default.GetBytes(_e.Message);
+                File.WriteAllBytes(_fileManager.TargetPath.Replace("ER0000.sl2", "Error.log"), err);
+            }
+            
         }
 
         private void exitButtonClick(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
 
         private void sourceFilePath_TextChanged(object sender, EventArgs e)
@@ -226,62 +275,9 @@ namespace EldenRingSaveCopy
 
         }
 
-        private void CreateFileBackup(string path, byte[] file)
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            int backupCount = 2;
-            string backupPath = path.Replace("ER0000.sl2", "ER0000.backup1");
-            while (File.Exists(backupPath))
-            {
-                backupPath = backupPath.Remove(backupPath.Length - 1, 1) + $"{backupCount}";
-                backupCount++;
-            }
-            File.WriteAllBytes(backupPath, file);
-        }
 
-        private void copyButton_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                CreateFileBackup(_fileManager.TargetPath, _fileManager.TargetFile);
-
-                byte[] newSave = new byte[_fileManager.SourceFile.Length];
-                byte[] slot = new byte[SLOT_LENGTH];
-
-                Array.Copy(_fileManager.TargetFile, newSave, _fileManager.TargetFile.Length);
-                Array.Copy(_fileManager.SourceFile, (int)_fileManager.SourceSlot, slot, 0, SLOT_LENGTH);
-                Array.Copy(slot, 0, newSave, (int)_fileManager.TargetSlot, SLOT_LENGTH);
-
-                foreach (int idLocation in newSave.StartingIndex(_fileManager.SourceID))
-                {
-                    Array.Copy(_fileManager.TargetID, 0, newSave, idLocation, _fileManager.TargetID.Length);
-                }
-
-                byte[] hash;
-                using (var md5 = MD5.Create())
-                {
-                    md5.ComputeHash(newSave, (int)_fileManager.TargetSlot, slot.Length);
-                    hash = md5.Hash;
-                }
-
-                Array.Copy(hash, 0, newSave, (int)_fileManager.TargetSlot - 16, 16);
-
-                File.WriteAllBytes(_fileManager.TargetPath, newSave);
-                File.Delete(_fileManager.TargetPath + ".bak");
-
-                copyButton.Enabled = false;
-                copyButton.Text = "Complete";
-                copyButton.BackColor = Color.SeaGreen;
-            }
-            catch (Exception _e)
-            {
-
-                copyButton.Enabled = false;
-                copyButton.Text = "Failed";
-                copyButton.BackColor = Color.DarkRed;
-                byte[] err = Encoding.Default.GetBytes(_e.Message);
-                File.WriteAllBytes(_fileManager.TargetPath.Replace("ER0000.sl2", "Error.log"), err);
-            }
-            
         }
     }
 }
